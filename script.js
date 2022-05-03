01999804369a5128bea1fab873a21d36cad946e3
Andy-git985 <andy.yu617@gmail.com>
2022-05-03 14:07:51 +0000

    8 kyu Sum of positive , completed

--- script.js
+++ script.js
@@ -108,3 +108,15 @@ function order(words){
               .sort((a, b) => extractNumber(a) - extractNumber(b))
   						.join(' ')
 }
+
+// 8 kyu Sum of positive
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+  return arr.filter(e => e > 0)
+            .reduce((p, c) => p + c, 0)
+}
