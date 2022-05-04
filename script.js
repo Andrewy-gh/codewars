5f343a4c42be6adc6c4e6e48ae8f7eebb8d36dd9
Andy-git985 <andy.yu617@gmail.com>
2022-05-04 14:15:39 +0000

    6kyu Two Sum completed

--- script.js
+++ script.js
@@ -109,26 +109,6 @@ function order(words){
   						.join(' ')
 }
 
-function twoSum(numbers, target) {
-	for (let i = 0; i < numbers.length; i++) {
-    const test = target - i
-    const arr = numbers.filter((elem, index) => {
-      if (index !== i && ) {
-        return elem
-      }
-    })
-    for (let num of arr) {
-      if (num === test) {
-        console.log(`${target} = ${i} + ${test}`)
-      }
-        
-    }
-    
-  }
-}
-
-
-twoSum([1234,5678,9012], 14690)
 // 8 kyu Sum of positive
 // You get an array of numbers, return the sum of all of the positives ones.
 
@@ -139,4 +119,32 @@ twoSum([1234,5678,9012], 14690)
 function positiveSum(arr) {
   return arr.filter(e => e > 0)
             .reduce((p, c) => p + c, 0)
+}
+
+// 6kyu Two Sum
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+// For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+// The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+// Based on: http://oj.leetcode.com/problems/two-sum/
+
+// twoSum [1, 2, 3] 4 === (0, 2)
+
+function twoSum(numbers, target) {
+	for (let i = 0; i < numbers.length; i++) {
+    const test = target - numbers[i]
+    console.log(test)
+    const arr = numbers.filter((elem, index) => {
+      if (index !== i) {
+        return elem
+      }
+    })
+    for (let num of arr) {
+      if (num === test) {
+        return i === numbers.indexOf(num) ? [parseInt(`${i}`), parseInt(`${numbers.lastIndexOf(num)}`)] : [parseInt(`${i}`), parseInt(`${numbers.indexOf(num)}`)] 
+      }
+    }
+  }
 }
