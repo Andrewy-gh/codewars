cb610974db0224f793b642407b81b848c0925a0b
Andy-git985 <andy.yu617@gmail.com>
2022-05-07 14:45:56 -0400

    6kyu array.diff completed

--- script.js
+++ script.js
@@ -217,3 +217,18 @@ function pyramid(num) {
   }
   return main
 }
+
+// 6kyu Array.diff
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+function arrayDiff(a, b) {
+  return a.filter(element => !b.includes(element))
+  }
