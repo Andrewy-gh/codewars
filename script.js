b01d595bd6a79c4f5f29c61970fe615caa654f58
Andy-git985 <andy.yu617@gmail.com>
2022-05-14 13:37:28 -0400

    7kyu Mumbling completed

--- script.js
+++ script.js
@@ -331,3 +331,24 @@ function createArrayOfTiers(num) {
       .map((e, i) => 
            String(num).substring(0, i + 1));
 }
+
+// 7 kyu Mumbling
+
+// This time no story, no theory. The examples below show you how to write function accum:
+// Examples:
+
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+  let arr = s.split('')
+  for (const [key, value] of Object.entries(arr)) {
+    for (let i = 0; i < key; i++) {
+      arr[key] += value.toLowerCase()
+    }
+  }
+  return arr.map(elem => elem[0].toUpperCase() + elem.substring(1)).join('-')
+}
