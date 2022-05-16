45406d22dc15b7f97fd46df8a638e7eb9665fc7e
Andy-git985 <andy.yu617@gmail.com>
2022-05-15 22:21:26 -0400

    8 kyu Calculate average completed

--- script.js
+++ script.js
@@ -384,3 +384,14 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   // code goes here
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
+
+// 8 kyu Calculate average
+
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+function find_average(array) {
+  const sum = array.reduce((a, b) => a + b, 0);
+  return array.length ? sum / array.length : 0;
+}
