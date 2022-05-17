de5548e56a51a2b13e136e665f7643054d5eff94
Andy-git985 <andy.yu617@gmail.com>
2022-05-16 23:12:11 -0400

    7 kyu Sum of a sequence completed

--- script.js
+++ script.js
@@ -395,3 +395,26 @@ function find_average(array) {
   const sum = array.reduce((a, b) => a + b, 0);
   return array.length ? sum / array.length : 0;
 }
+
+// 7 kyu Sum of a sequence
+
+// Your task is to make function, which returns the sum of a sequence of integers.
+
+// The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+
+// If begin value is greater than the end, function should returns 0
+
+// Examples
+
+// 2,2,2 --> 2
+// 2,6,2 --> 12 (2 + 4 + 6)
+// 1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+// 1,5,3  --> 5 (1 + 4)
+
+const sequenceSum = (begin, end, step) => {
+  let arr = [];
+  for (let i = begin; begin <= end; begin += step) {
+    arr.push(begin);
+  }
+  return arr.reduce((a, b) => a + b, 0);
+};
