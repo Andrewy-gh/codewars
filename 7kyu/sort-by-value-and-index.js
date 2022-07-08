fad385cbeceb5cf5ce83234714c5d4ab8bc9ea7c
Andy-git985 <andy.yu617@gmail.com>
2022-07-08 12:04:23 -0400

    7 kyu Sort an array by value and index completed

---
+++ 7kyu/sort-by-value-and-index.js
@@ -0,0 +1,31 @@
+// 7 kyu Sort an array by value and index
+// Your task is to sort an array of integer numbers by the product of the value and the index of the positions.
+
+// For sorting the index starts at 1, NOT at 0!
+// The sorting has to be ascending.
+// The array will never be null and will always contain numbers.
+
+// Example:
+
+// Input: 23, 2, 3, 4, 5
+// Product of value and index:
+// 23 => 23 * 1 = 23  -> Output-Pos 4
+//  2 =>  2 * 2 = 4   -> Output-Pos 1
+//  3 =>  3 * 3 = 9   -> Output-Pos 2
+//  4 =>  4 * 4 = 16  -> Output-Pos 3
+//  5 =>  5 * 5 = 25  -> Output-Pos 5
+
+// Output: 2, 3, 4, 23, 5
+
+function sortByValueAndIndex(array) {
+  const mapped = array.map((v, i) => {
+    return { i, value: v * (i + 1) };
+  });
+  mapped.sort((a, b) => a.value - b.value);
+  return mapped.map((v) => array[v.i]);
+}
+
+console.log(sortByValueAndIndex([1, 2, 3, 4, 5])); //[ 1, 2, 3, 4, 5 ];
+console.log(sortByValueAndIndex([23, 2, 3, 4, 5])); //[ 2, 3, 4, 23, 5 ];
+console.log(sortByValueAndIndex([26, 2, 3, 4, 5])); //[ 2, 3, 4, 5, 26 ];
+console.log(sortByValueAndIndex([9, 5, 1, 4, 3])); //[ 1, 9, 5, 3, 4 ];
