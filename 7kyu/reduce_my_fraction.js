c0519b06830c0b5d4d315e44cd6e4cb7e0d52fd4
Andy-git985 <andy.yu617@gmail.com>
2022-07-09 14:42:00 -0400

    7 kyu Reduce My Fraction completed

---
+++ 7kyu/reduce_my_fraction.js
@@ -0,0 +1,35 @@
+// 7 kyu Reduce My Fraction
+// Write a function which reduces fractions to their simplest form! Fractions will be presented as an array/tuple (depending on the language) of strictly positive integers, and the reduced fraction must be returned as an array/tuple:
+
+// input:   [numerator, denominator]
+// output:  [reduced numerator, reduced denominator]
+// example: [45, 120] --> [3, 8]
+
+// All numerators and denominators will be positive integers.
+
+// Note: This is an introductory Kata for a series... coming soon!
+
+function getGcf(a, b) {
+  var r = a % b;
+  while (r !== 0) {
+    a = b;
+    b = r;
+    r = a % b;
+  }
+  return b;
+}
+
+function reduce(fraction) {
+  const gcf = getGcf(fraction[0], fraction[1]);
+  const a = fraction[0] / gcf;
+  const b = fraction[1] / gcf;
+  return [a, b];
+}
+
+console.log(reduce([45, 120])); // [3, 8]
+console.log(reduce([60, 20])); // [3, 1]
+console.log(reduce([80, 120])); // [2, 3]
+console.log(reduce([4, 2])); // [2, 1]
+console.log(reduce([45, 120])); // [3, 8]
+console.log(reduce([1000, 1])); // [1000, 1]
+console.log(reduce([1, 1])); // [1, 1]
