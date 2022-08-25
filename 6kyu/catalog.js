e1a2a98d88509e77326c9967af62db4d0d5721db
Andy-git985 <andy.yu617@gmail.com>
2022-08-25 13:43:47 +0000

    6 kyu Catalog completed

---
+++ 6kyu/catalog.js
@@ -0,0 +1,46 @@
+// 6 kyu Catalog
+// You are given a small extract of a catalog:
+
+// s = "<prod><name>drill</name><prx>99</prx><qty>5</qty></prod>
+
+// <prod><name>hammer</name><prx>10</prx><qty>50</qty></prod>
+
+// <prod><name>screwdriver</name><prx>5</prx><qty>51</qty></prod>
+
+// <prod><name>table saw</name><prx>1099.99</prx><qty>5</qty></prod>
+
+// <prod><name>saw</name><prx>9</prx><qty>10</qty></prod>
+
+// ...
+
+// (prx stands for price, qty for quantity) and an article i.e "saw".
+
+// The function catalog(s, "saw") returns the line(s) corresponding to the article with $ before the prices:
+
+// "table saw > prx: $1099.99 qty: 5\nsaw > prx: $9 qty: 10\n..."
+
+// If the article is not in the catalog return "Nothing".
+// Notes
+
+//     There is a blank line between two lines of the catalog.
+//     The same article may appear more than once. If that happens return all the lines concerned by the article (in the same order as in the catalog).
+//     The line separator of results may depend on the language \nor \r\n. In Pascal \n is replaced by LineEnding.
+//     in Perl use "£" instead of "$" before the prices.
+//     You can see examples in the "Sample tests".
+
+function catalog(s, article) {
+  const lines = s.split('\n').filter(e => e)
+  const search = lines.filter(e => e.includes(article))
+  if (!search.length) return 'Nothing'
+  const result = search.map((e,i) => {
+    const name = search[i].split('name>')[1].slice(0, -2)
+    const prx = search[i].split('prx>')[1].slice(0, -2)
+    const qty = search[i].split('qty>')[1].slice(0, -2)
+    return `${name} > prx: $${prx} qty: ${qty}`
+  })
+  return result.join('\r\n')
+}
+
+console.log(catalog(s, 'ladder')) // 'ladder > prx: $112 qty: 12'
+console.log(catalog(s, 'saw')) // 'table saw > prx: $1099.99 qty: 5\r\nsaw > prx: $9 qty: 10\r\nsaw for metal > prx: $13.80 qty: 32'
+console.log(catalog(s, 'wood pallet')) // 'wood pallet > prx: $65 qty: 21'
