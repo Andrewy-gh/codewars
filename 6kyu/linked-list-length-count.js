30ce8e37c6638b5ead10511b5889aac89606275b
andy <andy.yu617@gmail.com>
2023-06-16 12:55:11 +0000

    6 kyu Linked Lists - Length & Count

---
+++ 6kyu/linked-list-length-count.js
@@ -0,0 +1,44 @@
+// 6 kyu
+// Linked Lists - Length & Count
+
+// Implement Length() to count the number of nodes in a linked list.
+
+// length(null) => 0
+// length(1 -> 2 -> 3 -> null) => 3
+
+// Implement Count() to count the occurrences of an integer in a linked list.
+
+// count(null, 1) => 0
+// count(1 -> 2 -> 3 -> null, 1) => 1
+// count(1 -> 1 -> 1 -> 2 -> 2 -> 2 -> 2 -> 3 -> 3 -> null, 2) => 4
+
+// I've decided to bundle these two functions within the same Kata since they are both very similar.
+
+// The push()/Push() and buildOneTwoThree()/BuildOneTwoThree() functions do not need to be redefined.
+
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function length(head) {
+  // Your code goes here.
+  let current = head;
+  let length = 0;
+  while (current) {
+    length++;
+    current = current.next;
+  }
+  return length;
+}
+
+function count(head, data) {
+  // Your code goes here.
+  let current = head;
+  let count = 0;
+  while (current) {
+    if (current.data === data) count++;
+    current = current.next;
+  }
+  return count;
+}
