2d002f05ec1a64b715fb5e92ed5639c21056d3f4
andy <andy.yu617@gmail.com>
2023-08-22 20:36:06 +0000

    6 kyu Grouped by commas

---
+++ 6kyu/grouped-by-commas.js
@@ -0,0 +1,35 @@
+// 6 kyu
+// Grouped by commas
+
+// Finish the solution so that it takes an input n (integer) and returns a string that is the decimal representation of the number grouped by commas after every 3 digits.
+
+// Assume: 0 <= n < 2147483647
+// Examples
+
+//        1  ->           "1"
+//       10  ->          "10"
+//      100  ->         "100"
+//     1000  ->       "1,000"
+//    10000  ->      "10,000"
+//   100000  ->     "100,000"
+//  1000000  ->   "1,000,000"
+// 35235235  ->  "35,235,235"
+
+function groupByCommas(n) {
+  return [...String(n)]
+    .reverse()
+    .map((e, i) =>
+      (i + 1) % 3 === 0 && i !== String(n).length - 1 ? `,${e}` : e
+    )
+    .reverse()
+    .join("");
+}
+
+var str = 12345;
+console.log(
+  [...String(str)]
+    .reverse()
+    .map((e, i) => ((i + 1) % 3 === 0 ? `,${e}` : e))
+    .reverse()
+    .join("")
+);
